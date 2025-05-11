8a27cbdff469847678bdb8fb43fb0449089fc732
Gökhan Yavuz <yavuzgokhan78@gmail.com>
2025-05-11 12:52:04 +0300

    Update Constants.java

--- app/src/main/java/com/example/cmp2004_final/Constants.java
+++ app/src/main/java/com/example/cmp2004_final/Constants.java
@@ -1,5 +1,5 @@
 package com.example.cmp2004_final;
 
 public class Constants {
-    public static final String GEMINI_API_KEY = "AIzaSyDBOpwzw9MfPxRts8OebZHoglhGwpIDZ2A";
+    public static final String GEMINI_API_KEY = "API_KEY";
 }
